6afb7744d5697ba7cc2ea9270ae5b17a00facd89
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-04-13 00:04:47 +0300

    clerk

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,10 +1,18 @@
 import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
 import ProfilePicture from "./_components/profile-picture";
+import { db } from "@/lib/db";
 
 type Props = {};
 
 const Settings = (props: Props) => {
+  const removeProfileImage = async () => {
+    "use server";
+    const response = await db.user.update({
+      where: {},
+    });
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
